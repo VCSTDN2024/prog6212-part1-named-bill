8f68d4c0d258dd4b66a53feb42b83586734c5823
named-bill <ST10445399@vcconnect.edu.za>
2025-09-17 23:14:08 +0300

    init: scaffold project (ASP.NET Core MVC)

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Submit() => View();
+        public IActionResult Approvals() => View(); // admin view
+    }
+}
